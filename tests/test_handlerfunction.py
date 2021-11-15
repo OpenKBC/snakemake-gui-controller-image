e9d5bc2f75bf67463f84b109463f99f48ff4d3df
Jun <swiri021@gmail.com>
2021-11-15 03:14:23 -0600

    Updated tests

--- tests/test_handlerfunction.py
+++ tests/test_handlerfunction.py
@@ -2,17 +2,17 @@ from handlers import yamlHandlers
 
 # Check callable status of yaml function
 def test_callFirstLayerValue():
-    resVal = yamlHandlers._parsing_yamlFile_omega(workflow_path="./", yamlfilename="dummy_config.yaml")
+    resVal = yamlHandlers._parsing_yamlFile_omega(workflow_path="tests/", yamlfilename="dummy_config.yaml")
     assert resVal ['InputFolder']=='inputFolder/inputfile.txt'
 
 def test_callSecondLayerValue():
-    resVal = yamlHandlers._parsing_yamlFile_omega(workflow_path="./", yamlfilename="dummy_config.yaml")
+    resVal = yamlHandlers._parsing_yamlFile_omega(workflow_path="tests/", yamlfilename="dummy_config.yaml")
     assert resVal['Output']['Path']=='outputFolder/'
 
 def test_callParsingFunction():
-    test_input = yamlHandlers._parsing_yamlFile(workflow_path=".", yamlfilename="dummy_config.yaml")
-    resVal = yamlHandlers._reform_yamlFile("./", test_input, "dummy_created")
-    with open("./dummy_config.yaml", "r") as testFile:
+    test_input = yamlHandlers._parsing_yamlFile(workflow_path="tests/", yamlfilename="dummy_config.yaml")
+    resVal = yamlHandlers._reform_yamlFile("tests/", test_input, "dummy_created")
+    with open("tests/dummy_config.yaml", "r") as testFile:
         with open(resVal, "r") as resultFile:
             testString = testFile.readlines()
             resultString = resultFile.readlines()
